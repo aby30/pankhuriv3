ac22fad53af40a17fbf9b8494e085df20677e033
Abhipray <abhipray.shailendra3@gmail.com>
2020-02-22 18:40:33 +0530

    BrowserRouter to HashBrowser

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 import Home from './modules/Home';
 import Favorite from './modules/Favorite';
 
