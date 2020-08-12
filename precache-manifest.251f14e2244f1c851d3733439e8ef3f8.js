a78f4f2435ae171d95f36741bb6f908f7345e8d5
Abhipray <abhipray.shailendra3@gmail.com>
2020-08-13 01:27:40 +0530

    Updates

---
+++ precache-manifest.251f14e2244f1c851d3733439e8ef3f8.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ec97aabd07d63ad4e67432f8d10fc1",
+    "url": "/pankhuriv3/index.html"
+  },
+  {
+    "revision": "acdff532a7e8620e5c49",
+    "url": "/pankhuriv3/static/css/main.6c96d3d3.chunk.css"
+  },
+  {
+    "revision": "b81717aa24f6f5f5b0d7",
+    "url": "/pankhuriv3/static/js/2.ef146d68.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/pankhuriv3/static/js/2.ef146d68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acdff532a7e8620e5c49",
+    "url": "/pankhuriv3/static/js/main.647b2fe1.chunk.js"
+  },
+  {
+    "revision": "3efd873748adb29acb16",
+    "url": "/pankhuriv3/static/js/runtime-main.f63a8a72.js"
+  },
+  {
+    "revision": "230be5cbbb2bbab4ede514fe0326d402",
+    "url": "/pankhuriv3/static/media/close.230be5cb.svg"
+  },
+  {
+    "revision": "6b655d1de01e194bcc53e436c5473aa2",
+    "url": "/pankhuriv3/static/media/mock_1.6b655d1d.jpg"
+  },
+  {
+    "revision": "928d95548c5646e69ae05531ddc94e4d",
+    "url": "/pankhuriv3/static/media/mock_10.928d9554.jpg"
+  },
+  {
+    "revision": "9a6f98e392b5b607fbc0b1dc575c0aff",
+    "url": "/pankhuriv3/static/media/mock_11.9a6f98e3.jpg"
+  },
+  {
+    "revision": "4054de9e2f17d03b7560d32e4a216763",
+    "url": "/pankhuriv3/static/media/mock_12.4054de9e.jpg"
+  },
+  {
+    "revision": "31b5a2fa8b942c31836a9c8b8dba5853",
+    "url": "/pankhuriv3/static/media/mock_13.31b5a2fa.jpg"
+  },
+  {
+    "revision": "da10a1b2e7189a30dba79d0eefaf6a77",
+    "url": "/pankhuriv3/static/media/mock_14.da10a1b2.jpg"
+  },
+  {
+    "revision": "158d2ebb616fc3426d0781ab088a9da1",
+    "url": "/pankhuriv3/static/media/mock_15.158d2ebb.jpg"
+  },
+  {
+    "revision": "158d2ebb616fc3426d0781ab088a9da1",
+    "url": "/pankhuriv3/static/media/mock_2.158d2ebb.jpg"
+  },
+  {
+    "revision": "dd702bc60938a0cfd487668c225c7978",
+    "url": "/pankhuriv3/static/media/mock_3.dd702bc6.jpg"
+  },
+  {
+    "revision": "7fcd7ea93ce07c1f30051b3b51c6e56c",
+    "url": "/pankhuriv3/static/media/mock_4.7fcd7ea9.jpg"
+  },
+  {
+    "revision": "a7a8216f411d05b8a16ffc47ddf656f8",
+    "url": "/pankhuriv3/static/media/mock_5.a7a8216f.jpg"
+  },
+  {
+    "revision": "38f6728d81b763bb47d2a049eedd1d7b",
+    "url": "/pankhuriv3/static/media/mock_6.38f6728d.jpg"
+  },
+  {
+    "revision": "96516cd12e6f00f2594d36ae297f8941",
+    "url": "/pankhuriv3/static/media/mock_7.96516cd1.jpg"
+  },
+  {
+    "revision": "f3c608fb6e4371fbcf26fd019cba95a6",
+    "url": "/pankhuriv3/static/media/mock_8.f3c608fb.jpg"
+  },
+  {
+    "revision": "9ee00eb2b4c599116040661dfa548683",
+    "url": "/pankhuriv3/static/media/mock_9.9ee00eb2.jpg"
+  },
+  {
+    "revision": "77269cf199ffa187b9140509fca1ad19",
+    "url": "/pankhuriv3/static/media/random.77269cf1.mp4"
+  },
+  {
+    "revision": "d1bef029d7523fa9402c36f1cf1a3606",
+    "url": "/pankhuriv3/static/media/savedHeart.d1bef029.png"
+  },
+  {
+    "revision": "e851e45bc4506602be52d8fffe47749b",
+    "url": "/pankhuriv3/static/media/shareLink.e851e45b.png"
+  }
+]);
--- src/components/common/helper.js
+++
@@ -1,4 +0,0 @@
-export const getCookie = (name) => {
-  var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-  if (match) return match[2];
-}
